99697d58582612606899c1e16320f82ec2e39f03
khodges <zkellyhodges@gmail.com>
2022-03-30 09:31:07 -0700

    changed fetch links again!

--- src/allEateriesContainer/allEateriesContainer.jsx
+++ src/allEateriesContainer/allEateriesContainer.jsx
@@ -39,7 +39,7 @@ const AllEateriesContainer = () =>{
     
             await console.log("new place\n", newPlace)
             // newPlace.img = await url
-            const createResponse = await fetch ('https://restaurant-app-front.herokuapp.com/restaurants',{
+            const createResponse = await fetch ('https://restaurant-app-back-end.herokuapp.com/restaurants',{
                 method: "POST",
                 body: JSON.stringify(newPlace),
                 headers: {
@@ -60,8 +60,7 @@ const AllEateriesContainer = () =>{
     //get all:
     const getEateries = async ()=>{
         try{
-            // const eateries = await fetch ('http://localhost:3001/restaurants')
-            const eateries = await fetch ('https://restaurant-app-front.herokuapp.com/restaurants')
+            const eateries = await fetch ('https://restaurant-app-back-end.herokuapp.com/restaurants')
             const parsedEateries = await eateries.json()
             setEateries(parsedEateries.data)
             // console.log(parsedEateries.data)
@@ -94,7 +93,7 @@ const AllEateriesContainer = () =>{
     //edit:
     const editOnePlace = async (idToEdit, placeToEdit)=>{
         try{
-            const editResponse = await fetch(`https://restaurant-app-front.herokuapp.com/restaurants/${idToEdit}`, {
+            const editResponse = await fetch(`https://restaurant-app-back-end.herokuapp.com/restaurants/${idToEdit}`, {
                 method:"PUT",
                 body:JSON.stringify(placeToEdit),
                 headers:{
@@ -119,7 +118,7 @@ const AllEateriesContainer = () =>{
     //delete: ISSUE: not updating list - state issue? use effect issue?
     const deletePlace = async (idToDelete) =>{
         try{
-            const deleteResponse = await fetch (`https://restaurant-app-front.herokuapp.com/restaurants/${idToDelete}`,{
+            const deleteResponse = await fetch (`https://restaurant-app-back-end.herokuapp.com/restaurants/${idToDelete}`,{
                 method: "DELETE"
             })
             const parsedDelete = await deleteResponse.json()
